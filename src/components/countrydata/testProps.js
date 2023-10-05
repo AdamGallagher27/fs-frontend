c7d6ccc47c8d10b0d6a7196dc1b19809863cde0b
Adam <N00211418@iadt.ie>
2023-10-05 22:34:29 +0100

    added test props

---
+++ src/components/countrydata/testProps.js
@@ -0,0 +1,18 @@
+
+// sample prop entry for country data test
+
+const testProps = {
+    name: {
+        common: 'Japan',
+    },
+    maps: {
+        googleMaps: 'https://goo.gl/maps/NGTLSCSrA8bMrvnX9',
+    },
+    flags: {
+        googleMaps: 'https://flagcdn.com/w320/jp.png',
+    },
+    area: 250000,
+    population: 10000000,
+    languages: ['Japanese'],
+}
+export default testProps
